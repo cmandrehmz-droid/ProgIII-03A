81c534c72939a06e1b750cac16de624376312fa9
AndrehMZ <cmandrehmz@gmail.com>
2026-04-13 09:22:13 -0500

    update

---
+++ Segundo Corte/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Gestión_de_Inventario_de_Productos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombreProducto = new System.Windows.Forms.TextBox();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numStockInicial = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numStockMinimo = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkEsPerecedero = new System.Windows.Forms.CheckBox();
+            this.rbReducido = new System.Windows.Forms.RadioButton();
+            this.rbGeneral = new System.Windows.Forms.RadioButton();
+            this.rbExento = new System.Windows.Forms.RadioButton();
+            this.dtpFechaVencimiento = new System.Windows.Forms.DateTimePicker();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.lblValidar = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numStockInicial)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStockMinimo)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(12, 52);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 22);
+            this.txtCodigo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Codigo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre Producto";
+            // 
+            // txtNombreProducto
+            // 
+            this.txtNombreProducto.Location = new System.Drawing.Point(158, 52);
+            this.txtNombreProducto.Name = "txtNombreProducto";
+            this.txtNombreProducto.Size = new System.Drawing.Size(100, 22);
+            this.txtNombreProducto.TabIndex = 3;
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Items.AddRange(new object[] {
+            "Electronica",
+            "Alimentos",
+            "Ropa"});
+            this.cmbCategoria.Location = new System.Drawing.Point(290, 52);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(112, 24);
+            this.cmbCategoria.TabIndex = 4;
+            this.cmbCategoria.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(287, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Categoria";
+            // 
+            // numStockInicial
+            // 
+            this.numStockInicial.Location = new System.Drawing.Point(444, 54);
+            this.numStockInicial.Name = "numStockInicial";
+            this.numStockInicial.Size = new System.Drawing.Size(120, 22);
+            this.numStockInicial.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(441, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Stock Inicial";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(597, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Stock Minimo";
+            // 
+            // numStockMinimo
+            // 
+            this.numStockMinimo.Location = new System.Drawing.Point(600, 54);
+            this.numStockMinimo.Name = "numStockMinimo";
+            this.numStockMinimo.Size = new System.Drawing.Size(120, 22);
+            this.numStockMinimo.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkEsPerecedero);
+            this.groupBox1.Controls.Add(this.rbReducido);
+            this.groupBox1.Controls.Add(this.rbGeneral);
+            this.groupBox1.Controls.Add(this.rbExento);
+            this.groupBox1.Location = new System.Drawing.Point(12, 103);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(256, 147);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "IVA";
+            // 
+            // chkEsPerecedero
+            // 
+            this.chkEsPerecedero.AutoSize = true;
+            this.chkEsPerecedero.Location = new System.Drawing.Point(135, 71);
+            this.chkEsPerecedero.Name = "chkEsPerecedero";
+            this.chkEsPerecedero.Size = new System.Drawing.Size(120, 20);
+            this.chkEsPerecedero.TabIndex = 11;
+            this.chkEsPerecedero.Text = "Es Perecedero";
+            this.chkEsPerecedero.UseVisualStyleBackColor = true;
+            this.chkEsPerecedero.CheckedChanged += new System.EventHandler(this.chkEsPerecedero_CheckedChanged);
+            // 
+            // rbReducido
+            // 
+            this.rbReducido.AutoSize = true;
+            this.rbReducido.Location = new System.Drawing.Point(19, 107);
+            this.rbReducido.Name = "rbReducido";
+            this.rbReducido.Size = new System.Drawing.Size(117, 20);
+            this.rbReducido.TabIndex = 2;
+            this.rbReducido.TabStop = true;
+            this.rbReducido.Text = "Reducido (5%)";
+            this.rbReducido.UseVisualStyleBackColor = true;
+            // 
+            // rbGeneral
+            // 
+            this.rbGeneral.AutoSize = true;
+            this.rbGeneral.Location = new System.Drawing.Point(19, 70);
+            this.rbGeneral.Name = "rbGeneral";
+            this.rbGeneral.Size = new System.Drawing.Size(113, 20);
+            this.rbGeneral.TabIndex = 1;
+            this.rbGeneral.TabStop = true;
+            this.rbGeneral.Text = "General (19%)";
+            this.rbGeneral.UseVisualStyleBackColor = true;
+            // 
+            // rbExento
+            // 
+            this.rbExento.AutoSize = true;
+            this.rbExento.Location = new System.Drawing.Point(19, 31);
+            this.rbExento.Name = "rbExento";
+            this.rbExento.Size = new System.Drawing.Size(99, 20);
+            this.rbExento.TabIndex = 0;
+            this.rbExento.TabStop = true;
+            this.rbExento.Text = "Exento (0%)";
+            this.rbExento.UseVisualStyleBackColor = true;
+            // 
+            // dtpFechaVencimiento
+            // 
+            this.dtpFechaVencimiento.Enabled = false;
+            this.dtpFechaVencimiento.Location = new System.Drawing.Point(290, 170);
+            this.dtpFechaVencimiento.Name = "dtpFechaVencimiento";
+            this.dtpFechaVencimiento.Size = new System.Drawing.Size(200, 22);
+            this.dtpFechaVencimiento.TabIndex = 11;
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(534, 152);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(186, 63);
+            this.btnValidar.TabIndex = 12;
+            this.btnValidar.Text = "VALIDAR ";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // lblValidar
+            // 
+            this.lblValidar.AutoSize = true;
+            this.lblValidar.Location = new System.Drawing.Point(587, 234);
+            this.lblValidar.Name = "lblValidar";
+            this.lblValidar.Size = new System.Drawing.Size(77, 16);
+            this.lblValidar.TabIndex = 13;
+            this.lblValidar.Text = "VALIDADO ";
+            this.lblValidar.Visible = false;
+            this.lblValidar.Click += new System.EventHandler(this.lblValidar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblValidar);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.dtpFechaVencimiento);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.numStockMinimo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numStockInicial);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.txtNombreProducto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCodigo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numStockInicial)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStockMinimo)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombreProducto;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numStockInicial;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numStockMinimo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbReducido;
+        private System.Windows.Forms.RadioButton rbGeneral;
+        private System.Windows.Forms.RadioButton rbExento;
+        private System.Windows.Forms.CheckBox chkEsPerecedero;
+        private System.Windows.Forms.DateTimePicker dtpFechaVencimiento;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Label lblValidar;
+    }
+}
+
---
+++ Segundo Corte/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestión_de_Inventario_de_Productos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkFechaVenci_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            bool validatecode = false;
+            bool validatestock = false;
+            bool validaterb = false;
+            bool validatenameproduct = false;
+            bool validatecategoria = false;
+            bool validated = false;
+
+            if (txtCodigo.Text.StartsWith("PROD-"))
+            {
+                validatecode = true;
+            }
+
+            if (numStockInicial.Value < numStockMinimo.Value)
+            {
+
+            }
+            else
+            {
+                validatestock = true;
+            }
+
+
+
+
+            if (rbGeneral.Checked)
+            {
+                validaterb = true;
+            }
+
+            if (rbReducido.Checked)
+            {
+                validaterb = true;
+            }
+
+            if (rbExento.Checked)
+            {
+                validaterb = true;
+            }
+
+
+            if (txtNombreProducto.Text == "")
+            {
+
+            }
+            else
+            {
+                validatenameproduct = true;
+            }
+
+
+            if (cmbCategoria.SelectedIndex == -1)
+            {
+
+            }
+            else
+            {
+                validatecategoria = true;
+            }
+
+
+
+            if (validatestock == true && validaterb == true && validatenameproduct == true && validatecode == true && validatecategoria == true)
+            {
+                validated = true;
+            }
+
+            if (validated == true)
+            {
+                lblValidar.Visible = true;
+                lblValidar.Text = "VALIDADO";
+            }
+            else
+            {
+                lblValidar.Visible = false;
+            }
+        }
+
+        private void chkEsPerecedero_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkEsPerecedero.Checked)
+            {
+                dtpFechaVencimiento.Enabled = true;
+            }
+        }
+
+        private void lblValidar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
